7e4bf561ab1cbef6f2a8e5e65e5966c5905e5a38
andres0sorio <osorio.af@gmail.com>
2016-03-17 13:12:24 -0500

    working version - full implementation

---
+++ PSP2Exercise02/src/main/java/StdDevTests.java
@@ -0,0 +1,71 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/** Copyright or License
+ *
+ */
+
+/**
+ * Package: 
+ *
+ * Class: StdDevTests StdDevTests.java
+ * 
+ * Original Author: @author AOSORIO
+ * 
+ * Description: [one line class summary]
+ * 
+ * Implementation: [Notes on implementation]
+ *
+ * Created: Mar 17, 2016 12:47:53 PM
+ * 
+ */
+public class StdDevTests {
+
+	@Test
+	public void testTable1() {
+		
+		final String dir = System.getProperty("user.dir");
+
+		String path = dir + "/data/";
+		String infile = path + "Tabla-1.csv";
+
+		RelSizeClassifier sd1 = new RelSizeClassifier("LOC/Method");
+		sd1.runClassification(path, infile, true);
+
+		double output = (double)sd1.getRelSizeRanges().getRange("VS");
+		assertEquals(4.3953, output , 0.0001 );
+		output = (double)sd1.getRelSizeRanges().getRange("S");
+		assertEquals(8.5081, output, 0.0001 );
+		output = (double)sd1.getRelSizeRanges().getRange("M");
+		assertEquals(16.4696, output, 0.0001 );
+		output = (double)sd1.getRelSizeRanges().getRange("L");
+		assertEquals(31.8811, output, 0.0001 );
+		output = (double)sd1.getRelSizeRanges().getRange("VL");
+		assertEquals(61.7137, output, 0.0001 );
+	}
+
+	@Test
+	public void testTable2() {
+		
+		final String dir = System.getProperty("user.dir");
+
+		String path = dir + "/data/";
+		String infile = path + "Tabla-2.csv";
+
+		RelSizeClassifier sd2 = new RelSizeClassifier("Pages/Chapter");
+		sd2.runClassification(path, infile, false);
+
+		double output = (double)sd2.getRelSizeRanges().getRange("VS");
+		assertEquals(6.3375, output , 0.0001 );
+		output = (double)sd2.getRelSizeRanges().getRange("S");
+		assertEquals(8.4393, output, 0.0001 );
+		output = (double)sd2.getRelSizeRanges().getRange("M");
+		assertEquals(11.2381, output, 0.0001 );
+		output = (double)sd2.getRelSizeRanges().getRange("L");
+		assertEquals(14.9650, output, 0.0001 );
+		output = (double)sd2.getRelSizeRanges().getRange("VL");
+		assertEquals(19.9280, output, 0.0001 );
+		
+	}
+}
