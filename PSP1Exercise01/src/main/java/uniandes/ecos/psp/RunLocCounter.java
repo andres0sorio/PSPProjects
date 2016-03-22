02e9ee8f8279dd26e4d7fd4001f5bf1b0614d57b
andres0sorio <osorio.af@gmail.com>
2016-03-22 17:32:32 -0500

    Adding a gui to LocCounter (experimental)

--- PSP1Exercise01/src/main/java/uniandes/ecos/psp/RunLocCounter.java
+++ PSP1Exercise01/src/main/java/uniandes/ecos/psp/RunLocCounter.java
@@ -28,22 +28,23 @@ public class RunLocCounter {
 	public static void main(String[] args) {
 
 		//FileChaser finder = new FileChaser("D:\\GIT\\PSPProjects");
-		FileChaser finder = new FileChaser("D:\\GIT\\PSPProjects\\PSP2Exercise02");
+		FileChaser finder = new FileChaser("D:\\GIT\\PSPProjects\\PSP2Exercise03");
 		//FileChaser finder = new FileChaser("D:\\GIT\\PSPProjects\\PSP1Exercise01\\test");
 		//FileChaser finder = new FileChaser("/home/aosorio/GIT/PSPProjects");
 		try {
+			
 			finder.processRoot();
-
 			ArrayList<String> java_files = finder.getAllFiles();
-
 			JavaCodeAnalyzer analyzer = new JavaCodeAnalyzer(java_files);
-
 			analyzer.beginJob();
 			analyzer.analyze();
 			analyzer.endJob();
+			
 		} catch (NullPointerException e) {
+			
 			System.out.println("You have a nullpointer exception! please check");
 			System.exit(1);
+			
 		}
 	}
 
