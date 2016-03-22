6a108d4432e10caa9b197e87b8fece1366b19b7f
andres0sorio <osorio.af@gmail.com>
2016-03-22 17:49:20 -0500

    Minor changes on TableReport

--- PSP2Exercise03/src/main/java/TableReport.java
+++ PSP2Exercise03/src/main/java/TableReport.java
@@ -2,7 +2,6 @@
  *
  */
 
-
 import java.util.ArrayList;
 
 /**
@@ -24,7 +23,9 @@ public class TableReport {
 	String name;
 	ArrayList<String> rows;
 
-	/** Constructor - takes a title for this table
+	/**
+	 * Constructor - takes a title for this table
+	 * 
 	 * @param name
 	 */
 	public TableReport(String name) {
@@ -33,7 +34,9 @@ public class TableReport {
 		rows = new ArrayList<String>();
 	}
 
-	/**  Add Row based on a pair string int value
+	/**
+	 * Add Row based on a pair string int value
+	 * 
 	 * @param description
 	 * @param value
 	 */
@@ -43,7 +46,9 @@ public class TableReport {
 		rows.add(add_row);
 	}
 
-	/** Add Row based on a pair string double value
+	/**
+	 * Add Row based on a pair string double value
+	 * 
 	 * @param description
 	 * @param x
 	 */
@@ -53,7 +58,9 @@ public class TableReport {
 		rows.add(add_row);
 	}
 
-	/** Add Row based on a string
+	/**
+	 * Add Row based on a string
+	 * 
 	 * @param string
 	 */
 	public void addRow(String string) {
@@ -61,28 +68,32 @@ public class TableReport {
 		rows.add(add_row);
 	}
 
-	/** Generate a nice HTML table
+	/**
+	 * Generate a nice HTML table
+	 * 
 	 * @return
 	 */
 	public String toHTML() {
 
-	    String table_report = "<style>#customers th {background-color:#4CAF50;color:white;}";
-	    table_report += "#customers td, #customers th {border: 1px solid #ddd;}</style>";
-	    table_report += "<table id=\"customers\"><tr>";
-	    table_report += "<th> TableReport " + name + "</th></tr><tr>";
-	    for (int nrow = 0; nrow < rows.size(); ++nrow) {
-		String item = rows.get(nrow).replace("\n", "");
-		String[] items = item.split("\t");
-		for (String td : items) {
-		    table_report += "<td>" + td + "</td>";
+		String table_report = "<style>#customers th {background-color:#4CAF50;color:white;}";
+		table_report += "#customers td, #customers th {border: 1px solid #ddd;}</style>";
+		table_report += "<table id=\"customers\"><tr>";
+		table_report += "<th> TableReport " + name + "</th></tr><tr>";
+		for (int nrow = 0; nrow < rows.size(); ++nrow) {
+			String item = rows.get(nrow).replace("\n", "");
+			String[] items = item.split("\t");
+			for (String td : items) {
+				table_report += "<td>" + td + "</td>";
+			}
+			table_report += "</tr><tr>";
 		}
-		table_report += "</tr><tr>";
-	    }
-	    table_report += "</table>";
-	    return table_report;
+		table_report += "</table>";
+		return table_report;
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
